afd3756eb28e67e3b80683547ee3005f5fd485e5
Gazok <jamiebayne@hotmail.com>
2014-10-14 19:56:14 +0100

    Added neighbour-getting

--- source/WaterAutomaton.hx
+++ source/WaterAutomaton.hx
@@ -7,11 +7,10 @@ class WaterAutomaton extends FlxSprite
 {
     static inline var WIDTH:Int = 100;
     static inline var HEIGHT:Int = 100;
-    static inline var RANGE:Int = 256;
+    static inline var RANGE:Int = 127;
     var map:Array<Array<Int>> = [];
 
-    public function new()
-    {
+    public function new() {
         super();
         for (i in 0 ... WIDTH) {
             map[i] = [];
@@ -22,11 +21,65 @@ class WaterAutomaton extends FlxSprite
         makeGraphic(WIDTH, HEIGHT, 0xFF<<24);
     }
 
-    public override function draw():Void
-    {
+    public override function update():Void {
+        var newMap:Array<Array<Int>> = [];
+
+        for (i in 0 ... WIDTH) {
+            newMap[i] = [];
+            for (j in 0 ... HEIGHT) {
+
+                var neighbourAverage:Int = 0;
+                var currentValue = map[i][j];
+
+                for (cell in fourNeighbours(i,j)) {
+                    if (cell != null) {
+                        neighbourAverage += cell;
+                    } else {
+                        neighbourAverage += currentValue;
+                    }
+                }
+
+                neighbourAverage = Std.int(neighbourAverage / 2);
+                newMap[i][j] = neighbourAverage - currentValue;
+                if (newMap[i][j] > RANGE) {
+                    newMap[i][j] = RANGE;
+                }
+                if (newMap[i][j] < -RANGE) {
+                    newMap[i][j] = -RANGE;
+                }
+            }
+        }
+
+        map = newMap;
+    }
+
+    public function triggerWave(_i:Int, _j:Int) {
+        if (_i >= 0 && _i < WIDTH  && _j >= 0 && _j < HEIGHT) {
+            map[_i][_j] += 1;
+        }
+    }
+
+    function fourNeighbours(_i:UInt, _j:UInt):Array<Null<Int>> {
+        var neighbours:Array<Null<Int>> = [null, null, null, null];
+        if (_i > 0) {
+            neighbours[0] = map[_i - 1][_j];
+        }
+        if (_i < WIDTH - 1) {
+            neighbours[2] = map[_i + 1][_j];
+        }
+        if (_j > 0) {
+            neighbours[1] = map[_i][_j - 1];
+        }
+        if (_j < HEIGHT - 1) {
+            neighbours[3] = map[_i][_j + 1];
+        }
+        return neighbours;
+    }
+
+    public override function draw():Void {
         for (i in 0 ... WIDTH) {
             for (j in 0 ... HEIGHT) {
-                var c = (map[i][j] + Std.int(RANGE / 2));
+                var c = map[i][j] + RANGE;
                 FlxSpriteUtil.drawRect(this, i, j, 1, 1,
                                        (0xFF << 24) + c * 0x10101);
             }
