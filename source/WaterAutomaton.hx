56eb97efe4c31929d5a7a49b60bfa5bfa64edb6c
Gazok <jamiebayne@hotmail.com>
2014-10-14 23:13:29 +0100

    Switched from newMap to lastMap

--- source/WaterAutomaton.hx
+++ source/WaterAutomaton.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxSprite;
+import flixel.FlxG;
 import flixel.util.FlxSpriteUtil;
 
 class WaterAutomaton extends FlxSprite
@@ -8,6 +9,8 @@ class WaterAutomaton extends FlxSprite
     static inline var WIDTH:Int = 100;
     static inline var HEIGHT:Int = 100;
     static inline var RANGE:Int = 127;
+    static inline var CELL_SIZE:Int = 25;
+    var lastMap:Array<Array<Int>> = [];
     var map:Array<Array<Int>> = [];
 
     public function new() {
@@ -19,17 +22,23 @@ class WaterAutomaton extends FlxSprite
             }
         }
         makeGraphic(WIDTH, HEIGHT, 0xFF<<24);
+        this.scale = new flixel.util.FlxPoint(CELL_SIZE, CELL_SIZE);
     }
 
     public override function update():Void {
-        var newMap:Array<Array<Int>> = [];
+        if (FlxG.mouse.pressed) {
+            triggerWave(20,20);
+        }
+
+        lastMap = map;
+        map = [];
 
         for (i in 0 ... WIDTH) {
-            newMap[i] = [];
+            map[i] = [];
             for (j in 0 ... HEIGHT) {
 
                 var neighbourAverage:Int = 0;
-                var currentValue = map[i][j];
+                var currentValue = lastMap[i][j];
 
                 for (cell in fourNeighbours(i,j)) {
                     if (cell != null) {
@@ -40,38 +49,46 @@ class WaterAutomaton extends FlxSprite
                 }
 
                 neighbourAverage = Std.int(neighbourAverage / 2);
-                newMap[i][j] = neighbourAverage - currentValue;
-                if (newMap[i][j] > RANGE) {
-                    newMap[i][j] = RANGE;
+                map[i][j] = neighbourAverage - currentValue;
+                if (map[i][j] > RANGE) {
+                    map[i][j] = RANGE;
                 }
-                if (newMap[i][j] < -RANGE) {
-                    newMap[i][j] = -RANGE;
+                if (map[i][j] < -RANGE) {
+                    map[i][j] = -RANGE;
+                }
+
+                if(map[i][j] != lastMap[i][j]) {
+                    //drawCell(i, j);
                 }
             }
         }
+    }
 
-        map = newMap;
+    function drawCell(_i, _j) {
+        var c = map[_i][_j] + RANGE;
+        FlxSpriteUtil.drawRect(this, _i, _j, 1, 1,
+                               (0xFF << 24) + c * 0x10101);
     }
 
     public function triggerWave(_i:Int, _j:Int) {
         if (_i >= 0 && _i < WIDTH  && _j >= 0 && _j < HEIGHT) {
-            map[_i][_j] += 1;
+            map[_i][_j] = -RANGE;
         }
     }
 
     function fourNeighbours(_i:UInt, _j:UInt):Array<Null<Int>> {
         var neighbours:Array<Null<Int>> = [null, null, null, null];
         if (_i > 0) {
-            neighbours[0] = map[_i - 1][_j];
+            neighbours[0] = lastMap[_i - 1][_j];
         }
         if (_i < WIDTH - 1) {
-            neighbours[2] = map[_i + 1][_j];
+            neighbours[2] = lastMap[_i + 1][_j];
         }
         if (_j > 0) {
-            neighbours[1] = map[_i][_j - 1];
+            neighbours[1] = lastMap[_i][_j - 1];
         }
         if (_j < HEIGHT - 1) {
-            neighbours[3] = map[_i][_j + 1];
+            neighbours[3] = lastMap[_i][_j + 1];
         }
         return neighbours;
     }
